66a1f97573de11f7bb168e82135d3a63ab0cdc1c
Merri1 <m121212@live.ie>
2023-03-03 22:03:12 +0000

    Fixed wildcard imports

--- src/main/java/com/fyp/tauceti/entity/Game.java
+++ src/main/java/com/fyp/tauceti/entity/Game.java
@@ -1,6 +1,14 @@
 package com.fyp.tauceti.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -24,7 +32,8 @@ public class Game {
     @JoinColumn(name = "OWNER", nullable = false)
     private SiteUser siteUser;
 
-    public Game() {}
+    public Game() {
+    }
 
     public Game(String name, String genre, LocalDateTime registrationDate, SiteUser siteUser) {
         this.name = name;
@@ -58,7 +67,8 @@ public class Game {
     }
 
     public String getgenre() {
-        return genre; }
+        return genre;
+    }
 
     public void setgenre(String genre) {
         this.genre = genre;
--- src/main/java/com/fyp/tauceti/entity/SiteUser.java
+++ src/main/java/com/fyp/tauceti/entity/SiteUser.java
@@ -1,6 +1,13 @@
 package com.fyp.tauceti.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -31,7 +38,8 @@ public class SiteUser {
     @OneToMany(mappedBy = "siteUser", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<Game> game;
 
-    public SiteUser() {}
+    public SiteUser() {
+    }
 
     public SiteUser(String firstName, String lastName, String email, String kNumber, LocalDateTime registrationDate) {
         this.firstName = firstName;
