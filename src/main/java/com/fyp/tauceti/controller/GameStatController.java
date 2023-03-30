2d1425c89e769698246f3278d4051e6688eb40dc
Merri1 <m121212@live.ie>
2023-03-30 01:46:00 +0100

    Create empty gameStat object when one doesn't exist

--- src/main/java/com/fyp/tauceti/controller/GameStatController.java
+++ src/main/java/com/fyp/tauceti/controller/GameStatController.java
@@ -2,6 +2,7 @@ package com.fyp.tauceti.controller;
 
 import com.fyp.tauceti.entity.Game;
 import com.fyp.tauceti.entity.GameStat;
+import com.fyp.tauceti.repository.GameRepository;
 import com.fyp.tauceti.repository.GameStatRepository;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,9 +12,11 @@ import java.util.UUID;
 @RestController
 public class GameStatController {
     private final GameStatRepository repository;
+    private final GameRepository gameRepository;
 
-    GameStatController(GameStatRepository repository) {
+    GameStatController(GameStatRepository repository, GameRepository gameRepository) {
         this.repository = repository;
+        this.gameRepository = gameRepository;
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
@@ -25,7 +28,26 @@ public class GameStatController {
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/game-stats/{id}")
     GameStat gameStat(@PathVariable UUID id) {
-        return repository.findGameStatByGame_Id(id);
+        GameStat gameStat = repository.findGameStatByGame_Id(id);
+
+        // If GameStat exisits for passed ID then return it
+        if(gameStat != null) {
+            return gameStat;
+        }
+        else {
+            // If not create a new empty GameStat record for the game ID that was passed
+            GameStat newGameStat = new GameStat();
+            newGameStat.setGame(gameRepository.findById(id));
+            newGameStat.setTotalPlayerCount(0L);
+            newGameStat.setTotalPlayTime(0L);
+            newGameStat.setWinCount(0L);
+            newGameStat.setLossCount(0L);
+            newGameStat.setTotalSessions(0L);
+
+            // Save new empty record and then return it
+            return repository.save(newGameStat);
+        }
+
     }
 
     @CrossOrigin(origins = "http://localhost:4200")
